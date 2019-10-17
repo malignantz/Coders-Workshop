4c52577840b06f54b517f871c4d3a94b008bc7be
malignantz <Garrett.Holmes@gmail.com>
2019-10-17 16:51:56 -0600

    passed test

--- Coding-Challenges/checkPermutation/checkPermutation.js
+++ Coding-Challenges/checkPermutation/checkPermutation.js
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
 
 const checkPermutation = (str1, str2) => {
-
+  if (str1.length !== str2.length) return false;
+  let dict = {};
+  str1.split("").forEach(letter => {
+    dict[letter] = dict[letter] ? dict[letter]++ : (dict[letter] = 0);
+  });
+  str2.split("").forEach(letter => {
+    if (dict[letter] > 0) {
+      dict[letter]--;
+    } else {
+      return false;
+    }
+  });
+  return true;
 };
 
 console.log(checkPermutation("god", "dog")); // true
