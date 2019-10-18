92ba5755c4dcde585fa38039749cf2a6ff3a419a
malignantz <Garrett.Holmes@gmail.com>
2019-10-17 20:32:01 -0600

    passed test

--- Coding-Challenges/keyMap/keyMap.js
+++ Coding-Challenges/keyMap/keyMap.js
@@ -1,10 +1,32 @@
 #!/usr/bin/env node
 
-const keyMap = keys => {
-
+let MAP = {
+  "2": ["a", "b", "c"],
+  "3": ["d", "e", "f"],
+  "4": ["g", "h", "i"],
+  "5": ["j", "k", "l"],
+  "6": ["m", "n", "o"],
+  "7": ["p", "q", "r", "s"],
+  "8": ["t", "u", "v"],
+  "9": ["w", "x", "y", "z"]
 };
 
-console.log(keyMap({
-  "2": ["a", "b", "c"],
-  "3": ["d", "e", "f"]
-})); // ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+const keyMap = (map, keys) => {
+  let words = [];
+  for (let key of String(keys).split("")) {
+    if (words.length === 0) {
+      words = map[key];
+    } else {
+      let letters = map[key];
+      let newWords = [];
+      for (let letter of letters) {
+        newWords.push(...words.map(word => word + letter));
+      }
+      words = newWords;
+    }
+  }
+  return words;
+};
+
+console.log(keyMap(MAP, 23)); // ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+console.log(keyMap(MAP, 5337).includes("keep"));
