f217ffe75c61d43f17b01a5157232bbbc6f6aa3f
malignantz <Garrett.Holmes@gmail.com>
2019-10-17 21:55:25 -0600

    passes tests

--- Coding-Challenges/fibonacciProduct/fibonacciProduct.js
+++ Coding-Challenges/fibonacciProduct/fibonacciProduct.js
@@ -1,7 +1,27 @@
 #!/usr/bin/env node
 
-const productFib = prod => {
-
+let makeFibSeq = () => {
+  let prev = 0;
+  let val = 1;
+  return () => {
+    let retVal = prev + val;
+    prev = val;
+    val = retVal;
+    return retVal;
+  };
 };
 
-console.log(productFib(5895));
+const productFib = prod => {
+  let getNextFib = makeFibSeq();
+  let fibNum = getNextFib();
+  let nextFib = getNextFib();
+  console.log(prod);
+  while (prod > fibNum * nextFib) {
+    fibNum = nextFib;
+    nextFib = getNextFib();
+  }
+  console.log(fibNum, nextFib, prod);
+  return [fibNum, nextFib, prod === fibNum * nextFib];
+};
+
+let deepEqual = (val1, val2) => JSON.stringify(val1) === JSON.stringify(val2);
