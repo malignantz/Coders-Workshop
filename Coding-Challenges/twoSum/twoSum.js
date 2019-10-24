1d03b3d84d8e9460caa6655ed7463ee8d739d8f7
malignantz <Garrett.Holmes@gmail.com>
2019-10-23 18:27:30 -0600

    passes tests

--- Coding-Challenges/twoSum/twoSum.js
+++ Coding-Challenges/twoSum/twoSum.js
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
 
-const twoSum = (nums, target) => {
+// [1,2,3,4,5], 9 => [3,4]
 
+const twoSum = (nums, target) => {
+  let indexLookUp = {};
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    let found = indexLookUp[num];
+    if (isFinite(found)) {
+      return [found, i];
+    } else {
+      indexLookUp[target - num] = i;
+    }
+  }
+  console.log("none found");
 };
 
-console.log(twoSum([2, 7, 11, 15], 9));  // [0, 1]
+console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
+console.log(twoSum([1, 2, 3, 4, 5], 9));
